e234b9943b822b9efd36fd34853ab6e24361f7af
Pablo <pabloduranp5@gmail.com>
2026-03-18 08:20:19 +0100

    Primer commit

---
+++ src/main/java/com/project/examenapirest/ExamenApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.project.examenapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApiRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenApiRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/examenapirest/ExamenApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.examenapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenApiRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
